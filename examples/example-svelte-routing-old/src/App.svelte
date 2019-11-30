6132553269dc2bde31faffb62eab52703909bacb
Nikos Katsikanis <nkatsikanis@gmail.com>
2019-11-30 14:10:37 +0000

    use fresh template for some unknown error

---
+++ examples/example-svelte-routing-old/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { Router, Route, link } from "svelte-routing";
+	import Signin from "./components/Signin.svelte";
+	import Home from "./components/Home.svelte";
+
+</script>
+
+<Router>
+	<h1>Svetle routing example</h1>
+	<a href="/login">Login</a>
+<hr>
+	<div>
+		<Route path="signin" component="{Signin}"/>
+		<Route path="/" component="{Home}"/>
+	</div>
+</Router>
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
