77f65b878ca0bf3b3f2dab0a73f201b75e1e7d89
Nikos Katsikanis <nkatsikanis@gmail.com>
2019-12-31 14:04:50 +0200

    🔥 firebase functions testing WIP

--- firebase_specific/functions/src/index.ts
+++ firebase_specific/functions/src/index.ts
@@ -6,3 +6,17 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+import * as admin from 'firebase-admin'
+
+admin.initializeApp();
+
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    const snapshot = await admin.database().ref('/messages').push({original: original});
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref.toString());
+});
