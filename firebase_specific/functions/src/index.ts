b3c1a08189d61f6a145c79eb8f6058653a422b53
Nikos Katsikanis <nkatsikanis@gmail.com>
2020-01-02 10:54:29 +0200

    add typescript watch

--- firebase_specific/functions/src/index.ts
+++ firebase_specific/functions/src/index.ts
@@ -11,12 +11,22 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp();
 
-
 export const addMessage = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     const original = req.query.text;
     // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    const snapshot = await admin.database().ref('/messages').push({original: original});
+    const snapshot = await admin.database().ref('/messages').push({ original: original });
     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
     res.redirect(303, snapshot.ref.toString());
+});
+
+export const makeUppercase = functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return snapshot.ref.parent.child('uppercase').set(uppercase);
 });
