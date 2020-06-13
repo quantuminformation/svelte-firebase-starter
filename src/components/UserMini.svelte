e53dadf69fd39dd581d1705eff8f67da4068011b
Nikos Katsikanis <nkatsikanis@gmail.com>
2020-06-13 18:37:20 +0100

    username display

--- src/components/UserMini.svelte
+++ src/components/UserMini.svelte
@@ -4,13 +4,11 @@
     //since this is in the authenticated HOC $authUserStore is guaranteed to be there
 
     export let user
-
-
 </script>
 
 <div style="border: 1px solid black;padding: 1em;margin-bottom: 1em">
     User:
     <a use:link href="{`profile?username=${user.username}`}">
-        <span>{user.displayName || user.uid}</span>
+        <span>{user.displayName || user.username || user.uid}</span>
     </a>
 </div>
