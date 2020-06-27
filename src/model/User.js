aa249a3097bf4bd192b49149c76128e4cd77b32b
Nikos Katsikanis <nkatsikanis@gmail.com>
2020-06-27 09:10:14 +0100

    removed unneeded model

--- src/model/User.js
+++
@@ -1,9 +0,0 @@
-// todo temping to switch to TS, iwll prob need need some logic at some point
-
-export function userFromFireBase(serverUser) {
-    return getUser(serverUser)
-}
-
-function getUser(user) {
-    return { email: user.email}
-}
