9f51b7c81863852dafa64116dd992c3f10a6eca4
Nikos Katsikanis <nkatsikanis@gmail.com>
2021-05-13 12:48:50 +0200

    video 24

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import firebaseAdapter from 'svelte-adapter-firebase';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,8 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: firebaseAdapter({ firebaseJson: '../firebase.json' }),
+
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
